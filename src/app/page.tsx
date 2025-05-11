0a264f8c427ac9cf52379978b1e2f99ce27aeef5
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2025-05-12 00:51:45 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { JSQuestionGenerator } from "@/components/js-question-generator"
 export default function Home() {
   return (
     <div className="container py-10 px-4 text-center block mx-auto">
-      {/* <div className="mb-10 space-y-4 text-center">
+      <div className="hidden sm:block mb-10 space-y-4 text-center">
         <h1 className="text-4xl font-bold tracking-tight">JavaScript Quiz Challenge</h1>
         <p className="mx-auto max-w-2xl text-muted-foreground">
           Test your JavaScript knowledge with randomly generated questions. Click the button below to get started!
         </p>
-      </div> */}
-      {/* On mobile disable this block */}
+      </div>
+
       <div className="flex justify-center">
         <JSQuestionGenerator />
       </div>
